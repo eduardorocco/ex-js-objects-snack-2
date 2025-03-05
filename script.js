0bf3f25b8cb747d074ce527776d496e145d30826
eduardorocco <eduardo_rocco@hotmail.it>
2025-03-05 10:06:31 +0100

    ex 6

--- script.js
+++ script.js
@@ -62,3 +62,54 @@
 // };
 
 // Output: per clonare chef il metodo migliore è lo spread operator, per clonare restaurant structuredClone
+
+
+// const hamburger = { 
+// 	name: "Cheese Burger", 
+// 	weight: 250,
+// 	maker: {
+// 		name: "Anonymous Chef",
+// 		restaurant: {
+// 			name: "Hyur's Burgers",
+// 			address: "Main Street, 123",
+// 			isOpen: true,
+// 		},
+// 		age: 29
+// 	}
+// };
+
+// const newRestaurant = {...hamburger.maker.restaurant};
+// newRestaurant.name = "Hyur's II";
+// newRestaurant.address = "Second Street, 12";
+// const secondBurger = {...hamburger};
+// secondBurger.maker.restaurant = newRestaurant;
+// secondBurger.maker.name = "Chef Hyur";
+
+// console.log(hamburger.maker.name); // Chef Hyur
+// console.log(secondBurger.maker.name); // Chef Hyur
+// console.log(hamburger.maker.restaurant.name); // Hyur's II
+// console.log(secondBurger.maker.restaurant.name); // Hyur's II
+
+const chef = {
+	name: "Chef Hyur",
+	age: 29,
+	makeBurger: (num = 1) => {
+		console.log(`Ecco ${num} hamburger per te!`);
+	},
+	restaurant: {
+		name: "Hyur's Burgers",
+		welcomeClient: () => {
+			console.log("Benvenuto!");
+		},
+		address: {
+			street: 'Main Street',
+			number: 123,
+			showAddress: () => {
+				console.log("Main Street 123");
+			}
+		},
+		isOpen: true,
+	}
+}
+
+// Output: il metodo migliore per l'oggetto chef è lo spread operator perché al suo interno ci sono delle funzioni 
