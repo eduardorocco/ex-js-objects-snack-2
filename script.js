ba89a6ac669dc5cf9ee7d82d6a23be3191e175bc
eduardorocco <eduardo_rocco@hotmail.it>
2025-03-05 09:59:52 +0100

    es.

--- script.js
+++ script.js
@@ -43,22 +43,22 @@
 
 // Output: 3 oggetti
 
-const chef = {
-	name: "Chef Hyur",
-	age: 29,
-	makeBurger: (num = 1) => {
-		console.log(`Ecco ${num} hamburger per te!`);
-	},
-}
+// const chef = {
+// 	name: "Chef Hyur",
+// 	age: 29,
+// 	makeBurger: (num = 1) => {
+// 		console.log(`Ecco ${num} hamburger per te!`);
+// 	},
+// }
 
-const restaurant = {
-	name: "Hyur's Burgers",
-	address: {
-		street: 'Main Street',
-		number: 123,
-	},
-	openingDate: new Date(2025, 3, 11),
-	isOpen: false,
-};
+// const restaurant = {
+// 	name: "Hyur's Burgers",
+// 	address: {
+// 		street: 'Main Street',
+// 		number: 123,
+// 	},
+// 	openingDate: new Date(2025, 3, 11),
+// 	isOpen: false,
+// };
 
 // Output: per clonare chef il metodo migliore è lo spread operator, per clonare restaurant structuredClone
